5e8c0816364ae4905020fe1f6ba40af24503e626
fdis111 <disubif@gmail.com>
2022-06-18 19:57:37 +0100

    feat: add personal bio in hp

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,8 +3,31 @@ import * as React from "react";
 const IndexPage = () => {
   return (
     <main>
-      <h1>Just a gatsby Blog</h1>
-      <a href="/blog">blog</a>
+      <h1>François Disubi </h1>
+      <p>Hey, I'm François Disubi, A dynamic full stack developer.</p>
+
+      <p>
+        I help developers to rise to incompetence and I accompany companies in
+        the implementation of digital tools to optimize their workflows.
+      </p>
+
+      <p>
+        Currently I work at Kinshasa Digital Academy as a full-stack developer
+        trainer. My tasks consist in :
+        <ul>
+          <li>Prepare trainings sessions,</li>
+          <li>Follow up on the learners skills development</li>
+          <li> And manage projects</li>
+        </ul>
+      </p>
+      <p>
+        In my free time (and even when I'm working), I love listening to music.
+      </p>
+      <p>
+        You can find me on GitHub, Linkedin, Instagram You can email me at
+        disubif@gmail.com
+      </p>
+      {/* <p>If you like my work, you can buy me a coffee.</p> */}
     </main>
   );
 };
--- src/pages/{MarkdownRemark.frontmatter__slug}.jsx
+++ src/pages/{MarkdownRemark.frontmatter__slug}.jsx
@@ -2,23 +2,14 @@ import React from "react";
 import { graphql } from "gatsby";
 
 export default function Template({ data }) {
-  const { markdownRemark } = data;
-  const { fronmatter, html } = markdownRemark;
-  console.log(fronmatter);
-
-  // const { title, date } = fronmatter;
+  const { html } = data.markdownRemark;
 
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        {/* <h1>{fronmatter.title}</h1> */}
-        {/* <h2>{fronmatter.date}</h2> */}
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
+    <div>
+      <div>
+        <div dangerouslySetInnerHTML={{ __html: html }} />
         <p>
-          <a href="/blog">Blog</a>
+          <a href="/stories">Stories</a>
         </p>
       </div>
     </div>
