e9d19117c7daaf7135cfe13ba2e8eaa3cd34b43d
fdis111 <disubif@gmail.com>
2022-06-18 18:28:42 +0100

    refactor: change js files to jsx

--- src/pages/{MarkdownRemark.frontmatter__slug}.jsx
+++ src/pages/{MarkdownRemark.frontmatter__slug}.jsx
@@ -3,13 +3,15 @@ import { graphql } from "gatsby";
 
 export default function Template({ data }) {
   const { markdownRemark } = data;
-  const { frontmatter, html } = markdownRemark;
+  const { fronmatter, html } = markdownRemark;
+
+  const { title, date } = fronmatter;
 
   return (
     <div className="blog-post-container">
       <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
+        <h1>{title}</h1>
+        <h2>{date}</h2>
         <div
           className="blog-post-content"
           dangerouslySetInnerHTML={{ __html: html }}
