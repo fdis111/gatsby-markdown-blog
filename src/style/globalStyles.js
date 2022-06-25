59ecc0c467998c2e2a89789508c54a6513ad9b6f
fdis111 <disubif@gmail.com>
2022-06-25 15:52:34 +0100

    fix: list identention

--- src/style/globalStyles.js
+++ src/style/globalStyles.js
@@ -3,7 +3,7 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
   *{
     margin: 0;
-    padding: 0;
+    padding: 0; 
     box-sizing: border-box;
   }
   main{
@@ -14,6 +14,12 @@ const GlobalStyle = createGlobalStyle`
   
   .page-content{
     padding: 4rem 0;
+  }
+
+  
+  ul, ol {
+    list-style-position: inside;
+    margin-left: 2em;
   }
 `;
 
